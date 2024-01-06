d48bd9bc362ea3e4d75ba98f6d17efc8fbeefecc
A-Cheng00 <151415017+A-Cheng00@users.noreply.github.com>
2024-01-06 16:36:45 +0800

    11

--- PI/temp6_3.py
+++ PI/temp6_3.py
@@ -56,12 +56,12 @@ def callback1(t:Timer):
     global start
     sensor = ADC(4)    
     vol = sensor.read_u16() * (3.3/65535)
-    temperature = 27 - (vol-0.706) / 0.001721
+    temperature = 40 - (vol-0.706) / 0.001721
     print(f'溫度:{temperature}')    
     delta = time.ticks_diff(time.ticks_ms(), start)
     print(delta)
     #溫度超過24度,並且發送alert()的時間已經大於60秒
-    if temperature >= 24 and delta >= 60 * 1000:        
+    if temperature >= 35 and delta >= 60 * 1000:        
         alert(temperature)
         start = time.ticks_ms()#重新設定計時的時間
         
