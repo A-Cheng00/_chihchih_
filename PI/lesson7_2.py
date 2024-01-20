a1ab0c3c3d860aa63df1eca42c9607e279ae61f7
A-Cheng00 <jerryhuang30426@hotmail.com>
2024-01-20 15:35:53 +0800

    7

--- PI/lesson7_2.py
+++ PI/lesson7_2.py
@@ -1,1 +1,19 @@
-gfgg
+import time
+from machine import Pin
+red_led = Pin(15,mode=Pin.OUT)
+#red_led.value(0)
+btn =Pin(14,mode=Pin.PULL_DOWN)
+is_press = False
+led_status = False
+
+while True:
+    if btn.value():
+        is_press=True
+        #red_led.value(1)
+        #print("按下")
+        #time.sleep_ms(500)
+    elif is_press:
+        print("release")
+        is_press=False
+        
+       # red_led.value(0)
