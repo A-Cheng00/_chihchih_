262a24f5f2514161f2d748d9086d9e359b30353d
A-Cheng00 <151415017+A-Cheng00@users.noreply.github.com>
2024-02-03 08:18:01 +0000

    9

--- GUI/main.py
+++ GUI/main.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
+import requests
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=5000)
 
 st.title("PP ")
 st.header("雞舍 :blue[溫度]& :red[光線]")
 st.divider()
+
+
+
+url = 'https://blynk.cloud/external/api/get?token=   &v0&v1'
+
+response = requests.request("GET",url)
+if response.status_code == 200:
+    all_data = response.json()
+    st.info(f'光線:{all_data["v0"]}')
+    st.warning(f'可變電阻:{all_data["v1"]}')
+else:
+    st.write("連線失敗,請等一下再試")
